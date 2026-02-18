1d5534eecbc429c8678ad65914831f83dfa76e8d
Toby Ang <58008827+tang0226@users.noreply.github.com>
2026-02-18 04:41:16 +0000

    Move credentials data to an external JSON file

--- index.js
+++ index.js
@@ -1,28 +1,18 @@
-const express = require('express');
-const path = require('path');
+const path = require("path");
+const fs = require("fs");
 
-const bcrypt = require('bcrypt');
+const bcrypt = require("bcrypt");
 const saltRounds = 10;
 
+const express = require("express");
 const app = express();
 const port = 3000;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-// Database stub
-const database = [
-  {
-    username: 'admin1',
-    email: 'admin@website.com',
-    password: '@dm1np@$$word123',
-  },
-  {
-    username: 'johndoe',
-    email: 'johndoe@example.com',
-    password: 'LorenIpsumDolorSit',
-  },
-];
+// Load the database
+const database = JSON.parse(fs.readFileSync("database.json", "utf8")).records;
 
 // Hash all passwords
 async function hashPasswords() {
@@ -34,45 +24,46 @@ async function hashPasswords() {
 const startServer = async () => {
   await hashPasswords();
 
-  app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, '/pages/index.html'));
+  app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, "/pages/index.html"));
   });
 
-  app.get('/login', (req, res) => {
-    res.sendFile(path.join(__dirname, '/pages/login.html'));
+  app.get("/login", (req, res) => {
+    res.sendFile(path.join(__dirname, "/pages/login.html"));
   });
 
-  app.post('/login', async (req, res) => {
+  app.post("/login", async (req, res) => {
     const { username, password } = req.body;
-    const userRecord = database.find(rec => rec.username === username || rec.email === username);
+    const userRecord = database.find(
+      (rec) => rec.username === username || rec.email === username,
+    );
 
     if (!userRecord) {
-      // Send 'invalid login' message back to client
+      // Send "invalid login" message back to client
       return res.json({
         error: true,
-        message: 'Invalid login / password',
+        message: "Invalid login / password",
       });
     }
-    
-    const isValid = await bcrypt.compare(password, userRecord.password)
+
+    const isValid = await bcrypt.compare(password, userRecord.password);
     if (isValid) {
       // Redirect user to dashboard
       return res.json({
         success: true,
-        redirect: '/dashboard',
+        redirect: "/dashboard",
       });
-    }
-    else {
-      // Send 'invalid login' message back to client
+    } else {
+      // Send "invalid login" message back to client
       return res.json({
         error: true,
-        message: 'Invalid login / password',
+        message: "Invalid login / password",
       });
     }
   });
 
-  app.get('/dashboard', (req, res) => {
-    res.sendFile(path.join(__dirname, '/pages/dashboard.html'));
+  app.get("/dashboard", (req, res) => {
+    res.sendFile(path.join(__dirname, "/pages/dashboard.html"));
   });
 
   app.listen(port, () => {
